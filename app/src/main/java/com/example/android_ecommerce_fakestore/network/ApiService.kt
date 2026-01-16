f3a86204055aa3474d1823f87301ea56238926f8
EmbyDumby <pierrette.malezieux@etu.unilasalle.Fr>
2026-01-16 14:55:31 +0100

    16/01/26 Récupération des catégories, tri par catégories.

--- app/src/main/java/com/example/android_ecommerce_fakestore/network/ApiService.kt
+++ app/src/main/java/com/example/android_ecommerce_fakestore/network/ApiService.kt
@@ -1,9 +1,20 @@
 //V1.0 - PIER - Init
-
+//V1.1 - PIER - Méthodes pour récupérer les catégories de produits et les produits par catégorie via l'API
+//v1.2 - PIER - Rectification categories recupération depuis liste des produits.
 package com.example.android_ecommerce_fakestore.network
+import com.example.android_ecommerce_fakestore.data.model.Product
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FakeStoreApiService {
     @GET("products")
     suspend fun getProducts(): List<com.example.android_ecommerce_fakestore.data.model.Product>
+    //V1.2--<<
+    // V1.1++<<
+    //@GET("products/categories")
+    //suspend fun getCategories(): List<String> // recup categories
+    //@GET("products/category/{category}")
+    //suspend fun getProductsByCategory(@Path("category") category: String): List<Product> //recup produits par categorie
+    //V1.1++>>
+    //V1.2-->>
 }
--- app/src/main/java/com/example/android_ecommerce_fakestore/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/android_ecommerce_fakestore/viewmodel/ProductViewModel.kt
@@ -1,6 +1,9 @@
 //V1.0 - PIER - Init
+//V1.1 - PIER - Recuperation categories
+
 package com.example.android_ecommerce_fakestore.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android_ecommerce_fakestore.data.model.Product
@@ -13,9 +16,52 @@ class ProductViewModel(private val repository: ProductRepository) : ViewModel() 
     private val _products = MutableStateFlow<List<Product>>(emptyList())
     val products: StateFlow<List<Product>> = _products
 
+    //v1.1++<<
+    private val _categories = MutableStateFlow<List<String>>(emptyList())
+    val categories: StateFlow<List<String>> = _categories
+    private val _filteredProducts = MutableStateFlow<List<Product>>(emptyList())
+    val filteredProducts: StateFlow<List<Product>> = _filteredProducts
+    //v1.1++>>
+
     fun fetchProducts(){
         viewModelScope.launch {
-            _products.value = repository.getProducts()
+            try {//test - a ne pas conserver
+                _products.value = repository.getProducts()
+                //test - a ne pas conserver<<
+                Log.d("ProductViewModel", "Produits chargés : ${_products.value}")
+                // Affiche la liste des produits dans les logs
+            } catch (e: Exception) {
+            Log.e("ProductViewModel", "Erreur lors du chargement des produits : ${e.message}")
+            // Affiche une erreur si le chargement échoue
         }
+            //test - a ne pas conserver >>
+        }
+
     }
+
+    //v1.1++<<
+    fun fetchCategories(){
+        try { // test - a ne pas garder
+            viewModelScope.launch{_categories.value = repository.getCategories()}
+            //test - a ne pas conserver <<
+            Log.d("ProductViewModel", "Catégories chargées : ${_categories.value}")
+            // Affiche la liste des catégories dans les logs
+        } catch (e: Exception) {
+            Log.e("ProductViewModel", "Erreur lors du chargement des catégories : ${e.message}")        // Affiche une erreur si le chargement échoue
+        } //test - a ne pas conserver >>
+    }
+    fun fetchGetProductsByCategory(category: String){
+        try { //test - a ne pas conserver
+            viewModelScope.launch {_filteredProducts.value = repository.getProductsByCategory(category)}
+            //test - a ne pas conserver <<
+            Log.d("ProductViewModel", "Produits filtrés (catégorie $category) : ${_filteredProducts.value}")
+            // Affiche la liste des produits filtrés dans les logs
+        } catch (e: Exception) {
+            Log.e("ProductViewModel", "Erreur lors du chargement des produits de la catégorie $category : ${e.message}")
+            // Affiche une erreur si le chargement échoue
+        }
+            //test - a ne pas conserver >>
+    }
+    //v1.1++>>
+
 }
