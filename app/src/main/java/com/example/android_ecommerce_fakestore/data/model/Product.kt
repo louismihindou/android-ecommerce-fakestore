5ae7b60b1a43dd3e89d8c0172f0dcd8d370b31ec
EmbyDumby <pierrette.malezieux@etu.unilasalle.Fr>
2026-01-16 11:32:55 +0100

    16/01/26 Product.kt v1.1 Rectification des champs de la classe Product.

--- app/src/main/java/com/example/android_ecommerce_fakestore/data/model/Product.kt
+++ app/src/main/java/com/example/android_ecommerce_fakestore/data/model/Product.kt
@@ -1,4 +1,5 @@
 //V1.0 - PIER - Init
+//v1.1 - PIER - Rectification champs produit
 
 package com.example.android_ecommerce_fakestore.data.model
 
@@ -7,14 +8,17 @@ import android.media.Rating
 data class Product (
     val id: Int,
     val title: String,
-    val price: Double,
+    //val price: Double, //v1.1--
+    val price: Float, //v1.1++
     val description: String,
     val category: String,
-    val image: String,
-    val rating: Rating
+    val image : String
+    //val rating: Rating //v1.1--
 )
 
-data class Rating(
-    val rate: Double,
-    val count: Int
-)
+//v1.1--<<
+//data class Rating(
+//    val rate: Double,
+//    val count: Int
+//)
+//v1.1-->>
